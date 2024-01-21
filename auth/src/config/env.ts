8ddb11ee67540dc8942bbece6b2cd75ccc641f05
mohamedelewasy <eng.m.elewasy@gmail.com>
2024-01-21 20:33:27 +0200

    fix index.ts conflict

--- auth/src/config/env.ts
+++ auth/src/config/env.ts
@@ -6,10 +6,10 @@ export const env = {
   port: +(process.env.PORT || 3000) as number,
   environment: process.env.NODE_ENV || 'development',
   mongoDb: {
-    url: process.env.MONGO_URL as string,
+    uri: process.env.MONGO_URI as string,
   },
 };
 
 export const checkEnvVariables = () => {
-  if (!env.mongoDb.url) throw new Error('env:MONGO_URL must be defined');
+  if (!env.mongoDb.uri) throw new Error('env:MONGO_URI must be defined');
 };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,14 @@
-<<<<<<< HEAD
-import { dbConnection } from "@duvdu-v1/duvdu";
-import { app } from "./app";
+import { dbConnection } from '@duvdu-v1/duvdu';
 
-
-const start = async () => {
-
-    if (!process.env.MONGO_URI) {
-        throw new Error("mongo uri must be defined");
-    };
-    
-    //connect to database
-    await dbConnection(process.env.MONGO_URI);
-
-    app.listen(3000, () => {
-        console.log("app listen in port 3000");
-    });
-};
-
-start();
-=======
-import { dbConnection } from './../config/mongo-connection';
 import { app } from './app';
+import { env, checkEnvVariables } from '../config/env';
 
 const start = async () => {
-  await dbConnection();
-  app.listen(3000, () => console.log('auth listen on port 3000'));
+  checkEnvVariables();
+  await dbConnection(env.mongoDb.url);
+  app.listen(3000, () => {
+    console.log('app listen on port 3000');
+  });
 };
 
 start();
->>>>>>> auth
