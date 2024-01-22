097a918abd26669125f9ce74d83df4a14c65c0ef
mohamedelewasy <eng.m.elewasy@gmail.com>
2024-01-22 23:23:28 +0200

    update

--- auth/src/controllers/__test__/signup.test.ts
+++ auth/src/controllers/__test__/signup.test.ts
@@ -6,12 +6,13 @@ const request = supertest(app);
 
 describe('AuthController', () => {
   it('should return 201 on valid signup', async () => {
-    const response = await request.post('/api/auth').send({
+    const response = await request.post('/api/users/signup').send({
       name: 'ewasy',
-      username: 'ewasy',
+      username: 'ewasy_mohamed',
       password: '123@Metoo',
       phoneNumber: { number: '01234567891' },
     });
+    console.log(response.body);
     expect(response.status).toBe(201);
     expect(response.body).toEqual({ message: 'success' });
     expect(Object.keys(response.body).length).toBe(1);
--- auth/src/controllers/auth/signup.controller.ts
+++ auth/src/controllers/auth/signup.controller.ts
@@ -1,16 +1,11 @@
 import 'express-async-errors';
 
-import { BadRequestError } from '@duvdu-v1/duvdu';
-
 import { Users } from '../../models/User.model';
 import { SignupHandler } from '../../types/endpoints';
 import { hashPassword } from '../../utils/bcrypt';
 import { generateToken } from '../../utils/generateToken';
 
-export const signupHandler: SignupHandler = async (req, res, next) => {
-  const user = await Users.findOne({ username: req.body.username });
-  if (user) return next(new BadRequestError('username already exists'));
-
+export const signupHandler: SignupHandler = async (req, res) => {
   const newUser = await Users.create({ ...req.body, password: hashPassword(req.body.password) });
   const token = generateToken(newUser.id);
   req.session = { jwt: token };
