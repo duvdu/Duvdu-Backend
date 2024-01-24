3912ca162f5433e52ad2f13a2de34638f2826c70
motemed-khaled <motemedkhaled5@gmail.com>
2024-01-24 21:26:16 +0200

    refact generic error

--- common/src/errors/generic-error.ts
+++ common/src/errors/generic-error.ts
@@ -1,17 +1,11 @@
 import { CustomError } from './custom-error';
-
-
-
-export class GenericError extends CustomError{
-  statusCode: number;
-
-  constructor(message:string , statusCode:number){
+export class GenericError extends CustomError {
+  statusCode = 406;
+  constructor(message: string) {
     super(message);
-    this.statusCode = statusCode;
-    Object.setPrototypeOf(this ,  GenericError.prototype);
+    Object.setPrototypeOf(this, GenericError.prototype);
   }
-
-  serializeError(): { message: string; field?: string | undefined; }[] {
-    return [{message:this.message}];
+  serializeError(): { message: string; field?: string | undefined }[] {
+    return [{ message: this.message }];
   }
 }
