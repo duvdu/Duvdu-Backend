00c2c6c603fb9b01454d293fa873fd38e8d2ffe0
motemed-khaled <motemedkhaled5@gmail.com>
2024-01-26 22:27:22 +0200

    add setup to test and test it

---
+++ auth/src/controllers/__test__/askResetPassword.test.ts
@@ -0,0 +1,33 @@
+import supertest from 'supertest';
+
+import { app } from '../../app';
+
+const request = supertest(app);
+
+describe('askResetPassword', () => {
+  it('should return 422 for invalid input', async () => {
+    await request.get('/api/users/reset-password').send({}).expect(422);
+
+    await request.get('/api/users/reset-password').send({ username: '' }).expect(422);
+
+    await request.get('/api/users/reset-password').send({ username: 'metoo' }).expect(422);
+  });
+  it('should return 404 if user not found', async () => {
+    await request.get('/api/users/reset-password').send({ username: 'metoooo' }).expect(404);
+  });
+  it('should return 200 for success', async () => {
+    await request
+      .post('/api/users/signup')
+      .send({
+        name: 'motemed khaled',
+        phoneNumber: {
+          number: '01022484942',
+        },
+        username: 'motemedKhaled',
+        password: '123@Metoo',
+      })
+      .expect(201);
+
+    await request.get('/api/users/reset-password').send({ username: 'motemedKhaled' }).expect(200);
+  });
+});
