ad45f2a13e0fbe716a7903bb0f00b839c16ded9c
motemed-khaled <motemedkhaled5@gmail.com>
2024-01-26 21:58:05 +0200

    add validation middleware to reset password

--- auth/src/validator/updatePhoneNumber.validation.ts
+++ auth/src/validator/updatePhoneNumber.validation.ts
@@ -4,9 +4,8 @@ import { body } from 'express-validator';
 export const updatePhoneNumberVal = [
   body('verificationCode')
     .notEmpty()
-    .withMessage('verificationCode required')
     .isLength({ min: 6, max: 6 })
     .withMessage('verificationCode must be 6 digit'),
-  body('phoneNumber').isNumeric().isMobilePhone(['ar-EG']).withMessage('invalid phone number'),
+  body('phoneNumber').isNumeric().isMobilePhone(['ar-EG']),
   globalValidatorMiddleware,
 ];
