4dd3f9cd18740fa7d968b005ef73b47aa67f255d
mohamedelewasy <eng.m.elewasy@gmail.com>
2024-01-30 20:44:40 +0200

    report endpoints

---
+++ auth/src/types/endpoints/report.endpoints.ts
@@ -0,0 +1,26 @@
+import { RequestHandler } from 'express';
+
+import { Ireport } from '../Report';
+
+type successResponse<T> = T & {
+  message: 'success';
+};
+
+export interface CreateReportHandler
+  extends RequestHandler<
+    unknown,
+    successResponse<unknown>,
+    Pick<Ireport, 'targetUser' | 'project' | 'desc' | 'attachments'>
+  > {}
+
+export interface UpdateReportHandler
+  extends RequestHandler<{ reportId: string }, successResponse<unknown>, Pick<Ireport, 'state'>> {}
+
+export interface RemoveReportHandler
+  extends RequestHandler<{ reportId: string }, successResponse<unknown>> {}
+
+export interface GetReportsHandler
+  extends RequestHandler<unknown, successResponse<{ data: Ireport[] }>> {}
+
+export interface GetReportHandler
+  extends RequestHandler<{ reportId: string }, successResponse<{ data: Ireport }>> {}
