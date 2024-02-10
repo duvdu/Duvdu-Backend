81b5f2cbc32ab424bba83626125592cfac806780
motemed-khaled <motemedkhaled5@gmail.com>
2024-02-10 20:18:22 +0200

    update get logged user response

--- auth/src/controllers/auth/get-logged-user-profile.controller.ts
+++ auth/src/controllers/auth/get-logged-user-profile.controller.ts
@@ -12,5 +12,5 @@ export const getLoggedUserProfileHandler: GetLoggedUserProfileHandler = async (r
   const averageRate = +(
     user.rate.ratersCounter > 0 ? user.rate.totalRates / user.rate.ratersCounter : 0
   ).toFixed(2);
-  res.status(200).json({ message: 'success', data: { ...user, averageRate } });
+  res.status(200).json({ message: 'success', data: { ...(user as any)._doc, averageRate } });
 };
