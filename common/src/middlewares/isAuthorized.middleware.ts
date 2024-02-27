fa72ed0f44c6ea5bd23958c60341f8adebc4a379
motemed-khaled <motemedkhaled5@gmail.com>
2024-02-27 21:22:15 +0200

    refact is authorized method and fix test cases

--- common/src/middlewares/isAuthorized.middleware.ts
+++ common/src/middlewares/isAuthorized.middleware.ts
@@ -5,10 +5,11 @@ import { UnauthorizedError } from '../errors/unauthorized-error';
 
 
 
-export const isAuthorized = (planModel:Model<any>,roleMode:Model<any> , permission:string)=><RequestHandler>(async (req,res,next)=>{
-  const plan = await planModel.findOne((req as any).loggedUser?.planId);
+export const isAuthorizedc = (planModel:Model<any>,roleMode:Model<any> , permission:string)=><RequestHandler>(async (req,res,next)=>{    
+  const plan = await planModel.findById((req as any).loggedUser?.planId);  
   if (!plan) return next(new UnauthorizedError('user dont have plain'));
-  const role = await roleMode.findOne(plan.role);
+  const role = await roleMode.findById(plan.role);
+  
   if (!role) return next(new UnauthorizedError('user dont have this role'));
 
   if (!role.features.includes(permission)) return next(new UnauthorizedError('user not allowed access this route'));
