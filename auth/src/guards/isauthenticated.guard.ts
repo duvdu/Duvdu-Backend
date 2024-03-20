b71fe30cb2e50692c01ccfcc90718f79a55f6d72
mohamedelewasy <eng.m.elewasy@gmail.com>
2024-03-20 23:35:13 +0200

    isauthenticated guard

---
+++ auth/src/guards/isauthenticated.guard.ts
@@ -0,0 +1,20 @@
+import { UnauthenticatedError } from '@duvdu-v1/duvdu';
+import { RequestHandler } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { env } from '../config/env';
+import { IjwtPayload } from '../types/JwtPayload';
+
+export const isauthenticated: RequestHandler = async (req, res, next) => {
+  if (!req.session.access) return next(new UnauthenticatedError('access token not found'));
+
+  let payload: IjwtPayload;
+  try {
+    payload = <IjwtPayload>verify(req.session.access, env.jwt.secret);
+    req.loggedUser = payload;
+  } catch (error) {
+    return res.status(423).json({ message: 'token expired' });
+  }
+
+  next();
+};
