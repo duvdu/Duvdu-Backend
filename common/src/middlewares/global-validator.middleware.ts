36b84e5c03cef678880dbdb9191dce6f4024f00f
mohamedelewasy <eng.m.elewasy@gmail.com>
2024-03-20 09:53:45 +0200

    update global validator to pass only matched data

--- common/src/middlewares/global-validator.middleware.ts
+++ common/src/middlewares/global-validator.middleware.ts
@@ -1,10 +1,13 @@
 import { RequestHandler } from 'express';
-import { validationResult } from 'express-validator';
+import { validationResult, matchedData } from 'express-validator';
 
 import { ValidationError } from '../errors/validation-error';
 
 export const globalValidatorMiddleware: RequestHandler = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) return next(new ValidationError(errors.array()));
+  req.body = matchedData(req, { locations: ['body'] });
+  req.params = matchedData(req, { locations: ['params'] });
+  req.query = matchedData(req, { locations: ['query'] });
   next();
 };
