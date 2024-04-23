7d8e718c7655c62b76eb33ef259f617f9ec6a6b8
motemed-khaled <motemedkhaled5@gmail.com>
2024-04-23 22:26:03 +0200

    refact all projects

--- common/src/models/allProjects.model.ts
+++ common/src/models/allProjects.model.ts
@@ -6,17 +6,18 @@ import { MODELS } from '../types/model-names';
 interface Iproject {
     project: {
         type: Schema.Types.ObjectId;
-        ref: string;
       };
+      ref: string;
 }
 
 
 export const Project = model<Iproject>(MODELS.projects , new Schema<Iproject>({
-  project:{
-    type:{
-      type:Schema.Types.ObjectId,
-      refPath: 'project.ref'
+  project: {
+    type: {
+      type: Schema.Types.ObjectId,
+      refPath: 'project.ref' // Dynamic reference based on project.ref field
     },
-    ref:String
-  }
+    ref: String
+  },
+  ref: String
 },{timestamps:true , collection:MODELS.projects}));
