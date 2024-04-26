e5ffc9576c06f825da3260c97dbf3df5ea2479af
motemed-khaled <motemedkhaled5@gmail.com>
2024-04-26 16:03:10 +0300

    log string connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,10 +6,10 @@ import { env, checkEnvVariables } from './config/env';
 
 const start = async () => {
   checkEnvVariables();
+  console.log(env.mongoDb.uri);
   await dbConnection(env.mongoDb.uri);
   app.listen(3000, async() => {
     console.log('app listen on port 3000');
-    console.log(env.mongoDb.uri);
     
     await appInit();
   });
