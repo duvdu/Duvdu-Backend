0e1ce4b219e4b78e8abb22ac0eef4d0d02a4973a
motemed-khaled <motemedkhaled5@gmail.com>
2024-04-26 17:22:03 +0300

    add final configration to connect to atlas database

--- copyrights/src/index.ts
+++ copyrights/src/index.ts
@@ -4,7 +4,6 @@ import { app } from './app';
 import { env, checkEnvVariables } from './config/env';
 
 const start = async () => {
-  console.log('test deploy');
   checkEnvVariables();
   await dbConnection(env.mongoDb.uri);
   app.listen(3000, () => {
