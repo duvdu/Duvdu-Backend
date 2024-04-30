96824f6f7c788f3bccfd5f791b4f4fc65866837f
motemed-khaled <motemedkhaled5@gmail.com>
2024-04-30 20:06:07 +0300

    refact portfolio-post issue and update permission

--- portfolio-post/src/index.ts
+++ portfolio-post/src/index.ts
@@ -3,8 +3,7 @@ import { dbConnection } from '@duvdu-v1/duvdu';
 import { app } from './app';
 import { env, checkEnvVariables } from './config/env';
 
-const start = async () => {
-  
+const start = async () => {  
   checkEnvVariables();
   await dbConnection(env.mongoDb.uri);
   app.listen(3000, () => {
