f75b708accd03d74fa0577836a129c071b18d3f6
motemed-khaled <motemedkhaled5@gmail.com>
2024-05-01 23:14:57 +0300

    refact redis conections

--- common/src/middlewares/redis-connection.ts
+++ common/src/middlewares/redis-connection.ts
@@ -1,24 +1,54 @@
+// import RedisStore from 'connect-redis';
+// import { createClient } from 'redis';
+
+// import { DatabaseConnectionError } from '../errors/data-base-connections';
+
+
+// export const redisConnection = (url: string , password:string , port:number) => {
+//   const client = createClient({
+//     password,
+//     socket:{
+//       host:url,
+//       port
+//     }
+//   });
+//   client
+//     .connect()
+//     .then(() => console.log(`redis connected in : ${url}`))
+//     .catch(() => {
+//       throw new DatabaseConnectionError(`cannot connect to redis : ${url}`);
+//     });
+//   return client;
+// };
+
+// export const sessionStore = (url: string , password:string , port:number) => new RedisStore({ client: redisConnection(url, password , port) });
 import RedisStore from 'connect-redis';
 import { createClient } from 'redis';
 
 import { DatabaseConnectionError } from '../errors/data-base-connections';
 
-
-export const redisConnection = (url: string , password:string , port:number) => {
+export const redisConnection = (url: string, password: string, port: number) => {
   const client = createClient({
     password,
     socket:{
-      host:url,
-      port
+      host: url,
+      port,
     }
   });
-  client
-    .connect()
-    .then(() => console.log(`redis connected in : ${url}`))
-    .catch(() => {
-      throw new DatabaseConnectionError(`cannot connect to redis : ${url}`);
-    });
+
+  client.on('connect', () => {
+    console.log(`Redis connected to ${url}:${port}`);
+  });
+
+  client.on('error', (err) => {
+    console.error(`Redis connection error: ${err}`);
+    throw new DatabaseConnectionError(`Cannot connect to Redis: ${err}`);
+  });
+
   return client;
 };
 
-export const sessionStore = (url: string , password:string , port:number) => new RedisStore({ client: redisConnection(url, password , port) });
+export const sessionStore = (url: string, password: string, port: number) => {
+  const client = redisConnection(url, password, port);
+  return new RedisStore({ client });
+};
