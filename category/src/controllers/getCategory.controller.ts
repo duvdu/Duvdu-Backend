f50171127916c6e9ab9834f7b6737a8b16e91391
motemed-khaled <motemedkhaled5@gmail.com>
2024-05-05 18:41:34 +0300

    add fiteration to catogry and update lang

--- category/src/controllers/getCategory.controller.ts
+++ category/src/controllers/getCategory.controller.ts
@@ -57,7 +57,6 @@ export const getCategoryHandler: GetCategoryHandler = async (req, res, next) => 
       },
     },
   ]);
-  console.log(category);
   if (!category) return next(new NotFound('category not found'));
   res.status(200).json({ message: 'success', data: category[0] });
 };
--- category/src/controllers/getCatogriesAdmin.controller.ts
+++ category/src/controllers/getCatogriesAdmin.controller.ts
@@ -1,9 +1,39 @@
 import { Categories } from '@duvdu-v1/duvdu';
+import { RequestHandler } from 'express';
 
 import { GetCatogriesAdminHandler } from '../types/endpoints/endpoints';
 
+export const getCategoriesAdminPagination: RequestHandler<unknown, unknown, unknown, {
+    search?: string;
+    title?: string;
+    cycle?: string;
+    status?: boolean;
+}> = async (req, res, next) => {
+    
+  req.pagination.filter = {};
+
+    
+  if (req.query.search) {
+    req.pagination.filter.$text = { $search: req.query.search };
+  }
+  if (req.query.title) {
+    const titleField = `title.${req.lang || 'en'}`; 
+    req.pagination.filter[titleField] = req.query.title;
+  }
+  if (req.query.cycle) {
+    req.pagination.filter.cycle = req.query.cycle;
+  }
+  if (req.query.status !== undefined) {
+    req.pagination.filter.status = req.query.status;
+  }
+  next();
+
+};
+
+
 export const getCatogriesAdminHandler: GetCatogriesAdminHandler = async (req, res) => {
   const category = await Categories.aggregate([
+    { $match: req.pagination.filter },
     {
       $project: {
         title: {
@@ -15,7 +45,6 @@ export const getCatogriesAdminHandler: GetCatogriesAdminHandler = async (req, re
         },
         _id: 1,
         creativesCounter: 1,
-        jobTitles: 1,
         cycle: 1,
         subCategories: {
           $map: {
@@ -51,6 +80,19 @@ export const getCatogriesAdminHandler: GetCatogriesAdminHandler = async (req, re
         updatedAt: 1,
         __v: 1,
         image: 1,
+        jobTitles: {
+          $map: {
+            input: '$jobTitles',
+            as: 'title',
+            in: {
+              $cond: {
+                if: { $eq: ['ar', req.lang] },
+                then: '$$title.ar',
+                else: '$$title.en',
+              },
+            },
+          },
+        },
       },
     },
   ]);
