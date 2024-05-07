e60af2959abe04c50f493b79f101ea54211940bb
motemed-khaled <motemedkhaled5@gmail.com>
2024-05-07 19:18:00 +0300

    refact models

--- common/src/models/portfolio-post.model.ts
+++ common/src/models/portfolio-post.model.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Types, Query } from 'mongoose';
+import { Schema, model, Types } from 'mongoose';
 
 import { MODELS } from '../types/model-names';
 import { Iuser } from '../types/User';
@@ -26,79 +26,66 @@ export interface IportfolioPost {
 }
 
 
-const PortfolioPostSchema = new Schema<IportfolioPost>(
-  {
-    user: { type: Schema.Types.ObjectId, ref: MODELS.user },
-    attachments: [String],
-    cover: { type: String, default: null },
-    title: { type: String, default: null },
-    desc: { type: String, default: null },
-    address: { type: String, default: null },
-    tools: [{ name: { type: String, default: null }, fees: { type: Number, default: 0 } }],
-    searchKeywords: [String],
-    creatives: [
-      {
-        creative: { type: Schema.Types.ObjectId, ref: MODELS.user },
-        fees: { type: Number, default: null },
+export const PortfolioPosts = model<IportfolioPost>(
+  MODELS.portfolioPost,
+  new Schema<IportfolioPost>(
+    {
+      user: { type: Schema.Types.ObjectId, ref: MODELS.user },
+      attachments: [String],
+      cover: { type: String, default: null },
+      title: { type: String, default: null },
+      desc: { type: String, default: null },
+      address: { type: String, default: null },
+      tools: [{ name: { type: String, default: null }, fees: { type: Number, default: 0 } }],
+      searchKeywords: [String],
+      creatives: [
+        {
+          creative: { type: Schema.Types.ObjectId, ref: MODELS.user },
+          fees: { type: Number, default: null },
+        },
+      ],
+      tags:[{ ar: { type: String, default: null }, en: { type: String, default: null } }],
+      subCategory:{
+        ar:String,
+        en:String
       },
-    ],
-    tags:[{ ar: { type: String, default: null }, en: { type: String, default: null } }],
-    subCategory:{
-      ar:String,
-      en:String
-    },
-    projectBudget: { type: Number, default: null },
-    category: { type: Schema.Types.ObjectId, ref: MODELS.category, required: true },
-    projectScale: { scale: { type: Number, default: 0 }, time: { type: String, default: null } },
-    showOnHome: { type: Boolean, default: true },
-    cycle: { type: Number, default: 1 },
-    isDeleted: { type: Boolean, default: false },
-    rate: {
-      ratersCounter: { type: Number, default: 0 },
-      totalRates: { type: Number, default: 0 },
-    },
-  },
-  {
-    timestamps: true,
-    collection: MODELS.portfolioPost,
-    toJSON: {
-      transform(doc, ret) {
-        if (ret.cover) ret.cover = process.env.BUCKET_HOST + '/' + ret.cover;
-        if (ret.attachments)
-          ret.attachments = ret.attachments.map(
-            (el: string) => process.env.BUCKET_HOST + '/' + el,
-          );
+      projectBudget: { type: Number, default: null },
+      category: { type: Schema.Types.ObjectId, ref: MODELS.category, required: true },
+      projectScale: { scale: { type: Number, default: 0 }, time: { type: String, default: null } },
+      showOnHome: { type: Boolean, default: true },
+      cycle: { type: Number, default: 1 },
+      isDeleted: { type: Boolean, default: false },
+      rate: {
+        ratersCounter: { type: Number, default: 0 },
+        totalRates: { type: Number, default: 0 },
       },
     },
-  },
-)
-  .index({ createdAt: 1, updatedAt: -1 })
-  .index({ title: 'text', desc: 'text', tools: 'text', searchKeywords: 'text' });
+    {
+      timestamps: true,
+      collection: MODELS.portfolioPost,
+      toJSON: {
+        transform(doc, ret) {
+          if (ret.cover) ret.cover = process.env.BUCKET_HOST + '/' + ret.cover;
+          if (ret.attachments)
+            ret.attachments = ret.attachments.map(
+              (el: string) => process.env.BUCKET_HOST + '/' + el,
+            );
+        },
+      },
+    },
+  )
+    .index({ createdAt: 1, updatedAt: -1 })
+    .index({ title: 'text', desc: 'text', tools: 'text', searchKeywords: 'text' }),
+);
 
-
-function transformDocument(doc : IportfolioPost) {
-  if (doc.cover) {
-    doc.cover = process.env.BUCKET_HOST + '/' + doc.cover;
+PortfolioPosts.schema.set('toJSON', {
+  transform: function (doc, ret) {
+    if (ret.cover) {
+      ret.cover = process.env.BUCKET_HOST + '/' + ret.cover;
+    }
+    if (ret.attachments) {
+      ret.attachments = ret.attachments.map((el: string) => process.env.BUCKET_HOST + '/' + el);
+    }
+    return ret;
   }
-  if (doc.attachments) {
-    doc.attachments = doc.attachments.map(el => process.env.BUCKET_HOST + '/' + el);
-  }
-}
-
-PortfolioPostSchema.pre('save', function(next) {
-  transformDocument(this);
-  next();
 });
-
-PortfolioPostSchema.pre(/^find/, function(next) {
-  const query = this  as Query<IportfolioPost[], IportfolioPost>;
-
-  query.then((documents : IportfolioPost[]) => {
-    documents.forEach((doc) => {
-      transformDocument(doc);
-    });
-    next();
-  }).catch(next);
-});
-
-export const PortfolioPosts = model<IportfolioPost>(  MODELS.portfolioPost, PortfolioPostSchema);
