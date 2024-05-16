e2e7226e78ddd62f79490a73e247b49906f22d66
ewasy <eng.m.elewasy@gmail.com>
2024-05-16 15:01:42 +0300

    contracts base structure

---
+++ contracts/src/types/custom-definition.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import { IjwtPayload, Ipagination } from '@duvdu-v1/duvdu';
+
+declare module 'express-session' {
+  interface SessionData {
+    access: string;
+    refresh: string;
+  }
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      loggedUser: IjwtPayload;
+      pagination: Ipagination;
+      lang: 'en'|'ar'
+    }
+  }
+}
