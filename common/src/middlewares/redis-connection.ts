a22e13656ca4da7dc0eac889c001318cce3ee6ea
motemed-khaled <motemedkhaled5@gmail.com>
2024-05-19 13:03:44 +0300

    init redis connection

--- common/src/middlewares/redis-connection.ts
+++ common/src/middlewares/redis-connection.ts
@@ -4,16 +4,19 @@ import { createClient } from 'redis';
 import { DatabaseConnectionError } from '../errors/data-base-connections';
 
 
-export const redisConnection = async (url: string , password:string) => {
-  const client = createClient({ url , password });
-  await client
-    .connect()
-    .then(() => console.log(`redis connected in : ${url}`))
-    .catch(() => {
-      throw new DatabaseConnectionError(`cannot connect to redis : ${url}`);
-    });
-  return client;
+export const redisConnection = async (url: string, password: string) => {
+  try {
+    const client = createClient({ url, password });
+    await client.connect();
+    console.log(`Redis connected in : ${url}`);
+    return client;
+  } catch (error) {
+    console.error(`Cannot connect to Redis: ${url}`, error);
+    throw new DatabaseConnectionError(`Cannot connect to Redis: ${url}`);
+  }
 };
 
-export const sessionStore = (url: string , password:string) => new RedisStore({ client: redisConnection(url , password) });
-
+export const sessionStore = async (url: string, password: string) => {
+  const client = await redisConnection(url, password);
+  return new RedisStore({ client });
+};
