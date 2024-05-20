3499b12bfd21041880d20cf13d0599d5fc67e449
ewasy <eng.m.elewasy@gmail.com>
2024-05-20 07:32:30 +0300

    fix chat & notification sort

--- chat/src/controllers/notification/getLoggedUserNotification.controller.ts
+++ chat/src/controllers/notification/getLoggedUserNotification.controller.ts
@@ -4,12 +4,14 @@ import { Notification } from '@duvdu-v1/duvdu';
 
 import { GetLoggedUserNotificationHandler } from '../../types/endpoints/notification.endpoint';
 
-
-export const getLoggedUserNotificationHandler:GetLoggedUserNotificationHandler = async (req,res)=>{
+export const getLoggedUserNotificationHandler: GetLoggedUserNotificationHandler = async (
+  req,
+  res,
+) => {
   const notifications = await Notification.find({ targetUser: req.loggedUser?.id })
     .limit(req.pagination.limit)
     .skip(req.pagination.skip)
-    .sort({ watchied: 1, createdAt: 1 })
+    .sort({ watchied: -1, createdAt: -1 })
     .populate('sourceUser', 'name username profileImage');
 
   const unWatchiedCount = await Notification.countDocuments({
@@ -20,13 +22,13 @@ export const getLoggedUserNotificationHandler:GetLoggedUserNotificationHandler =
   const resultCount = await Notification.countDocuments({ targetUser: req.loggedUser?.id });
 
   res.status(200).json({
-    message:'success',
+    message: 'success',
     pagination: {
       currentPage: req.pagination.page,
       resultCount,
       totalPages: Math.ceil(resultCount / req.pagination.limit),
     },
-    data:notifications,
-    unWatchiedCount:unWatchiedCount
+    data: notifications,
+    unWatchiedCount: unWatchiedCount,
   });
 };
