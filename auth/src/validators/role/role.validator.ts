2a5c1ca7ff3ebe35a34298766966c5b62092addd
mohamed elewasy <59490414+mohamedelewasy@users.noreply.github.com>
2024-05-26 13:48:13 +0300

    Update role.validator.ts

--- auth/src/validators/role/role.validator.ts
+++ auth/src/validators/role/role.validator.ts
@@ -5,19 +5,19 @@ import { body, param } from 'express-validator';
 
 export const create = [
   body('key').exists().isString().trim().isLength({ min: 2 }).withMessage('keyLength'),
-  body('features').exists().isArray({ min: 1 }).withMessage('featuresArrayLength'),
-  body('features.*').custom((val) => {
+  body('permissions').exists().isArray({ min: 1 }).withMessage('featuresArrayLength'),
+  body('permissions.*').custom((val) => {
     if (Object.values(PERMISSIONS).includes(val)) return true;
-    throw new Error(`${val} not a feature`);
+    throw new Error(`${val} not a permission`);
   }).withMessage('featureInvalid'),
   globalValidatorMiddleware,
 ];
 
 export const update = [
-  body('features').exists().isArray({ min: 1 }).withMessage('featuresArrayLength'),
-  body('features.*').custom((val) => {
+  body('permissions').exists().isArray({ min: 1 }).withMessage('featuresArrayLength'),
+  body('permissions.*').custom((val) => {
     if (Object.values(PERMISSIONS).includes(val)) return true;
-    throw new Error(`${val} not a feature`);
+    throw new Error(`${val} not a permission`);
   }).withMessage('featureInvalid'),
   globalValidatorMiddleware,
 ];
