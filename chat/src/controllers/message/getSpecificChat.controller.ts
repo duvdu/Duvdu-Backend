1432b65029e0208e9c5b5746093da00555a8cfdc
motemed-khaled <motemedkhaled5@gmail.com>
2024-06-04 21:33:20 +0300

    fix chat issue

--- chat/src/controllers/message/getSpecificChat.controller.ts
+++ chat/src/controllers/message/getSpecificChat.controller.ts
@@ -9,132 +9,6 @@ export const getSpecificChatHandler: GetSpecificChatHandler = async (req, res) =
   const userTwo = new Types.ObjectId(req.loggedUser.id);
   const userOne = new Types.ObjectId(req.params.receiver);
 
-  // const chat = await Message.aggregate([
-  //   {
-  //     $match: {
-  //       $or: [
-  //         { sender: userOne, receiver: userTwo },
-  //         { sender: userTwo, receiver: userOne },
-  //       ],
-  //     },
-  //   },
-  //   {
-  //     $group: {
-  //       _id: null,
-  //       allMessages: { $push: '$$ROOT' },
-  //     },
-  //   },
-  //   {
-  //     $addFields: {
-  //       unreadMessageCount: {
-  //         $size: {
-  //           $filter: {
-  //             input: '$allMessages',
-  //             as: 'message',
-  //             cond: {
-  //               $and: [
-  //                 { $eq: ['$$message.receiver', userTwo] },
-  //                 { $eq: ['$$message.watched', false] },
-  //               ],
-  //             },
-  //           },
-  //         },
-  //       },
-  //     },
-  //   },
-  //   {
-  //     $unwind: '$allMessages',
-  //   },
-  //   {
-  //     $lookup: {
-  //       from: 'users',
-  //       localField: 'allMessages.sender',
-  //       foreignField: '_id',
-  //       as: 'senderDetails',
-  //     },
-  //   },
-  //   {
-  //     $lookup: {
-  //       from: 'users',
-  //       localField: 'allMessages.receiver',
-  //       foreignField: '_id',
-  //       as: 'receiverDetails',
-  //     },
-  //   },
-  //   {
-  //     $addFields: {
-  //       'allMessages.sender': {
-  //         $cond: [
-  //           { $eq: [{ $size: '$senderDetails' }, 0] },
-  //           null,
-  //           { $arrayElemAt: ['$senderDetails', 0] },
-  //         ],
-  //       },
-  //       'allMessages.receiver': {
-  //         $cond: [
-  //           { $eq: [{ $size: '$receiverDetails' }, 0] },
-  //           null,
-  //           { $arrayElemAt: ['$receiverDetails', 0] },
-  //         ],
-  //       },
-  //     },
-  //   },
-  //   {
-  //     $addFields: {
-  //       'allMessages.sender': {
-  //         $cond: [
-  //           { $eq: ['$allMessages.sender', null] },
-  //           null,
-  //           {
-  //             profileImage: {
-  //               $ifNull: [
-  //                 { $concat: [process.env.BUCKET_HOST, '/', '$allMessages.sender.profileImage'] },
-  //                 null,
-  //               ],
-  //             },
-  //             isOnline: { $ifNull: ['$allMessages.sender.isOnline', null] },
-  //             username: { $ifNull: ['$allMessages.sender.username', null] },
-  //             name: { $ifNull: ['$allMessages.sender.name', null] },
-  //             _id: { $ifNull: ['$allMessages.sender._id', null] },
-  //           },
-  //         ],
-  //       },
-  //       'allMessages.receiver': {
-  //         $cond: [
-  //           { $eq: ['$allMessages.receiver', null] },
-  //           null,
-  //           {
-  //             profileImage: {
-  //               $ifNull: [
-  //                 { $concat: [process.env.BUCKET_HOST, '/', '$allMessages.receiver.profileImage'] },
-  //                 null,
-  //               ],
-  //             },
-  //             isOnline: { $ifNull: ['$allMessages.receiver.isOnline', null] },
-  //             username: { $ifNull: ['$allMessages.receiver.username', null] },
-  //             name: { $ifNull: ['$allMessages.receiver.name', null] },
-  //             _id: { $ifNull: ['$allMessages.receiver._id', null] },
-  //           },
-  //         ],
-  //       },
-  //     },
-  //   },
-  //   {
-  //     $replaceRoot: {
-  //       newRoot: { $mergeObjects: ['$allMessages', { unreadMessageCount: '$unreadMessageCount' }] },
-  //     },
-  //   },
-  //   {
-  //     $sort: { createdAt: -1 },
-  //   },
-  //   {
-  //     $skip: req.pagination.skip,
-  //   },
-  //   {
-  //     $limit: req.pagination.limit,
-  //   },
-  // ]);
-
 
   const chat = await Message.aggregate([
     {
@@ -245,18 +119,24 @@ export const getSpecificChatHandler: GetSpecificChatHandler = async (req, res) =
           ],
         },
         'allMessages.media': {
-          $map: {
-            input: '$allMessages.media',
-            as: 'mediaItem',
-            in: {
-              type: '$$mediaItem.type',
-              url: {
-                $ifNull: [
-                  { $concat: [process.env.BUCKET_HOST, '/', '$$mediaItem.url'] },
-                  null,
-                ],
+          $cond: {
+            if: { $isArray: '$allMessages.media' },
+            then: {
+              $map: {
+                input: '$allMessages.media',
+                as: 'mediaItem',
+                in: {
+                  type: '$$mediaItem.type',
+                  url: {
+                    $ifNull: [
+                      { $concat: [process.env.BUCKET_HOST, '/', '$$mediaItem.url'] },
+                      null,
+                    ],
+                  },
+                },
               },
             },
+            else: [],
           },
         },
       },
@@ -276,6 +156,7 @@ export const getSpecificChatHandler: GetSpecificChatHandler = async (req, res) =
       $limit: req.pagination.limit,
     },
   ]);
+  
   
 
 
