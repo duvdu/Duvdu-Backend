a780a4c7a33424a23702a5496b5ea5ec06b99f86
m.elewasy <eng.m.elewasy@gmail.com>
2024-06-04 12:04:16 +0300

    fix get logged user profile response

--- auth/src/controllers/auth/get-logged-user-profile.controller.ts
+++ auth/src/controllers/auth/get-logged-user-profile.controller.ts
@@ -1,14 +1,19 @@
-import { UnauthenticatedError , Users } from '@duvdu-v1/duvdu';
+import { UnauthenticatedError, Users } from '@duvdu-v1/duvdu';
 
 import { GetLoggedUserProfileHandler } from '../../types/endpoints/user.endpoints';
 
 export const getLoggedUserProfileHandler: GetLoggedUserProfileHandler = async (req, res, next) => {
-  const user = await Users.findById(req.loggedUser?.id).select(
-    '-googleId -appleId -password -verificationCode.code -verificationCode.expireAt -token -role',
-  );
-  if (!user) return next(new UnauthenticatedError(undefined , req.lang));
-  const averageRate = +(
+  const user = await Users.findById(req.loggedUser?.id)
+    .select(
+      '-googleId -appleId -password -verificationCode.code -verificationCode.expireAt -token -role -favourites',
+    )
+    .lean();
+  if (!user) return next(new UnauthenticatedError(undefined, req.lang));
+  (user as any).averageRate = +(
     user.rate.ratersCounter > 0 ? user.rate.totalRates / user.rate.ratersCounter : 0
   ).toFixed(2);
-  res.status(200).json({ message: 'success', data: { ...(user as any)._doc, averageRate } });
+
+  if (user.coverImage) user.coverImage = process.env.BUCKET_HOST + '/' + user.coverImage;
+  if (user.profileImage) user.profileImage = process.env.BUCKET_HOST + '/' + user.profileImage;
+  res.status(200).json({ message: 'success', data: user });
 };
