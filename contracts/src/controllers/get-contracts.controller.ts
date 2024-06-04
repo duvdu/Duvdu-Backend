b2fa60212db67bbcc649045d1f02642aac571e05
m.elewasy <eng.m.elewasy@gmail.com>
2024-06-04 11:48:14 +0300

    contracts population

--- contracts/src/controllers/get-contracts.controller.ts
+++ contracts/src/controllers/get-contracts.controller.ts
@@ -1,4 +1,5 @@
-import { Contracts, Icontract, SuccessResponse } from '@duvdu-v1/duvdu';
+import { Contracts, Icontract, Iuser, SuccessResponse } from '@duvdu-v1/duvdu';
+import { ContractStatus } from '@duvdu-v1/duvdu/build/models/contracts.model';
 import { RequestHandler } from 'express';
 
 export const getContracts: RequestHandler<
@@ -12,7 +13,19 @@ export const getContracts: RequestHandler<
   else if (req.query.filter === 'i_received') filter.targetUser = req.loggedUser.id;
   else filter.$or = [{ sourceUser: req.loggedUser.id }, { targetUser: req.loggedUser.id }];
 
-  const contracts = await Contracts.find(filter);
+  const contracts = await Contracts.find(filter)
+    .populate([{ path: 'targetUser', select: 'username name profileImage isOnline' }])
+    .lean();
+  contracts.forEach((contract) => {
+    if ((contract.targetUser as Iuser).profileImage)
+      (contract.targetUser as Iuser).profileImage =
+        process.env.BUCKET_HOST + '/' + (contract.targetUser as Iuser).profileImage;
+    if (contract.status !== ContractStatus.pending) return contract;
+    const createdAt = new Date(contract.createdAt).getTime();
+    const responseNoticePeriod = createdAt + 24 * 60 * 60 * 1000;
+    (contract as any).remainingTime = parseInt(`${(responseNoticePeriod - Date.now()) / 1000}`);
+    return contract;
+  });
   res.status(200).json({
     message: 'success',
     data: contracts,
