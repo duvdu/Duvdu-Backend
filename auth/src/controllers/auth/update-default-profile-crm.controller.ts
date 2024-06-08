8f0709bb2f64bce1a2f6a3d0d4853112a78b1610
m.elewasy <eng.m.elewasy@gmail.com>
2024-06-09 00:53:06 +0300

    update default profile & cover images

--- auth/src/controllers/auth/update-default-profile-crm.controller.ts
+++ auth/src/controllers/auth/update-default-profile-crm.controller.ts
@@ -1,24 +1,44 @@
 import { BadRequestError, Bucket, SuccessResponse, Users, redisClient } from '@duvdu-v1/duvdu';
 import { RequestHandler } from 'express';
 
-export const updateDefaultProfileCrm: RequestHandler<
+export const updateDefaultImagesCrm: RequestHandler<
   unknown,
   SuccessResponse<{ data: { url: string } }>
 > = async (req, res, next) => {
-  if (!req.file) return next(new BadRequestError(undefined , req.lang));
-  const bucket = new Bucket();
-  // TODO: remove last image
-  await bucket.saveBucketFiles('defaults', req.file);
-  const oldDefaultProfile = (await redisClient.get('default_profile')) || 'defaults/profile.jpg';
-  await bucket.removeBucketFiles(oldDefaultProfile);
-  await redisClient.set('default_profile', 'defaults/' + req.file.filename);
+  if (!req.files) return next(new BadRequestError(undefined, req.lang));
+  const profileImage: Express.Multer.File | undefined = (req.files as any).profile?.[0];
+  const coverImage: Express.Multer.File | undefined = (req.files as any).cover?.[0];
 
-  await Users.updateMany(
-    { profileImage: oldDefaultProfile },
-    { profileImage: 'defaults/' + req.file.filename },
-  );
+  const bucket = new Bucket();
+
+  if (profileImage) {
+    await bucket.saveBucketFiles('defaults', profileImage);
+    const oldDefaultProfile = (await redisClient.get('default_profile')) || 'defaults/profile.jpg';
+    await bucket.removeBucketFiles(oldDefaultProfile);
+    await redisClient.set('default_profile', 'defaults/' + profileImage.filename);
+    await Users.updateMany(
+      { profileImage: oldDefaultProfile },
+      { profileImage: 'defaults/' + profileImage.filename },
+    );
+  }
+  if (coverImage) {
+    await bucket.saveBucketFiles('defaults', coverImage);
+    const oldDefaultCover = (await redisClient.get('default_cover')) || 'defaults/cover.jpg';
+    await bucket.removeBucketFiles(oldDefaultCover);
+    await redisClient.set('default_cover', 'defaults/' + coverImage.filename);
+    await Users.updateMany(
+      { coverImage: oldDefaultCover },
+      { coverImage: 'defaults/' + coverImage.filename },
+    );
+  }
+
   res.status(200).json({
     message: 'success',
-    data: { url: process.env.BUCKET_HOST + '/defaults/' + req.file.filename },
+    data: {
+      url:
+        process.env.BUCKET_HOST +
+          '/defaults/' +
+          (coverImage ? coverImage?.filename : profileImage?.filename) || '',
+    },
   });
 };
