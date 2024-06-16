9490e80cb2a0d6a473c0e1bdf1ebb24281a04fd4
motemed-khaled <motemedkhaled5@gmail.com>
2024-06-16 12:55:29 +0300

    update producer model

--- common/src/models/producer.model.ts
+++ common/src/models/producer.model.ts
@@ -1,11 +1,17 @@
 import { model, Schema, Types } from 'mongoose';
 
+import { Icategory } from '../types/Category';
 import { CYCLES } from '../types/cycles';
 import { MODELS } from '../types/model-names';
 import { Iuser } from '../types/User';
 
 export interface Iproducer{
-    user : Types.ObjectId | Iuser
+    user : Types.ObjectId | Iuser;
+    category: Types.ObjectId | Icategory;
+    subCategories?: { title: { en: string; ar: string }; tags: { en: string; ar: string }[] }[];
+    maxBudget:number;
+    minBudget:number;
+    searchKeywords:string []
 }
 
 export interface IproducerBooking{
@@ -105,5 +111,13 @@ export const Producer = model<Iproducer>(MODELS.producer , new Schema<Iproducer>
   user:{
     type: Schema.Types.ObjectId,
     ref:MODELS.user
-  }
+  },
+  category:{
+    type:Schema.Types.ObjectId,
+    ref:MODELS.category
+  },
+  subCategories:[{title:{ar:{type:String , default:null} , en:{type:String , default:null}} , tags:[{en:{type:String , default:null } , ar:{type:String , default:null}}]}],
+  minBudget:{type:Number , default:null},
+  maxBudget:{type:Number , default:null},
+  searchKeywords:[String]
 },{timestamps:true , collection:MODELS.producer}));
