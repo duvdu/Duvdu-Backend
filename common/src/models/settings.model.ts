47fa8a5f161ed9ad751790b2485517bf71423010
motemed-khaled <motemedkhaled5@gmail.com>
2024-06-16 21:08:27 +0300

    add setting model

---
+++ common/src/models/settings.model.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from 'mongoose';
+
+import { MODELS } from '../types/model-names';
+
+
+
+
+export interface Isetting {
+    expirationTime:{time:number}[]
+}
+
+
+export const Setting = model<Isetting>(MODELS.setting , new Schema<Isetting>({
+  expirationTime:[{time:Number , default:null}]
+},{timestamps:true , collection:MODELS.setting}));
