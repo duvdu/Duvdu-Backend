2e8441f24ff6759a9b895b4eb6d0848aa649deb2
mohamedelewasy <eng.m.elewasy@gmail.com>
2024-06-17 11:19:20 +0300

    fix crashed server

--- common/src/models/settings.model.ts
+++ common/src/models/settings.model.ts
@@ -2,14 +2,16 @@ import { model, Schema } from 'mongoose';
 
 import { MODELS } from '../types/model-names';
 
-
-
-
 export interface Isetting {
-    expirationTime:{time:number}[]
+  expirationTime: { time: number }[];
 }
 
-
-export const Setting = model<Isetting>(MODELS.setting , new Schema<Isetting>({
-  expirationTime:[{time:Number , unique:true}]
-},{timestamps:true , collection:MODELS.setting}));
+export const Setting = model<Isetting>(
+  MODELS.setting,
+  new Schema<Isetting>(
+    {
+      expirationTime: [{ time: { type: Number, unique: true } }],
+    },
+    { timestamps: true, collection: MODELS.setting },
+  ),
+);
