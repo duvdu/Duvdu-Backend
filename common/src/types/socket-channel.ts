5b20343f99a67420a2fbb5e6df70f089089843c7
motemed-khaled <motemedkhaled5@gmail.com>
2024-06-29 16:12:12 +0300

    add notification details and socket for project contract

--- common/src/types/socket-channel.ts
+++ common/src/types/socket-channel.ts
@@ -2,5 +2,7 @@ export enum Channels {
     new_message = 'new_message',
     new_follower = 'new_follower',
     new_producer_contract = 'new_producer_contract',
-    updated_producer_contract = 'updated_producer_contract'
+    updated_producer_contract = 'updated_producer_contract',
+    new_project_contract = 'new_project_contract',
+    update_project_contract = 'update_project_contract',
   }
