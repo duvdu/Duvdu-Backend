d05baeeb5917778bacfbf0a0fde2a506d305a1e4
motemed-khaled <motemedkhaled5@gmail.com>
2024-07-18 23:19:46 +0300

    update user model to handle multiple tokens and handle isauthenticated middleware also

--- common/src/middlewares/auth.middleware.ts
+++ common/src/middlewares/auth.middleware.ts
@@ -21,30 +21,40 @@ export const isauthenticated: RequestHandler = async (req, res, next) => {
         en: `Forbidden: User is blocked ${(req as any).loggedUser.isBlocked.reason}`,
         ar:` ممنوع: المستخدم محظور ${(req as any).loggedUser.isBlocked.reason}`,
       } , (req as any).lang));
-  } catch (error) {
-    // return res.status(423).json({ message: 'access token expired' });
-    const user = await Users.findOne({token:(req as any).session.refresh});
-    if (!user) 
-      return res.status(423).json({ message: 'refresh token expired' });
-
-    const role = await Roles.findById(user.role);
-    if (!role) 
-      return res.status(423).json({ message: 'invalid role' });
-
-    const accessToken = generateAccessToken({
-      id: user.id,
-      isVerified: user.isVerified,
-      isBlocked: user.isBlocked,
-      role: { key: role.key, permissions: role.permissions },
-    });
-
-    (req as any).session.access = accessToken;
-    (req as any).session.refresh = user.token;
-
     next();
+  } catch (error) {
+    try {
+      const payload = <{id:string}>verify((req as any).session.refresh, process.env.JWT_KEY!);
+      const user = await Users.findById(payload.id);
+      if (!user) 
+        return res.status(423).json({ message: 'token expired' });
 
+      if (user.isBlocked.value)
+        return next(new UnauthorizedError( {
+          en: `Forbidden: User is blocked ${(req as any).loggedUser.isBlocked.reason}`,
+          ar:` ممنوع: المستخدم محظور ${(req as any).loggedUser.isBlocked.reason}`,
+        } , (req as any).lang));
+
+      const role = await Roles.findById(user.role);
+      if (!role) 
+        return res.status(423).json({ message: 'invalid role' });
+
+      const accessToken = generateAccessToken({
+        id: user.id,
+        isVerified: user.isVerified,
+        isBlocked: user.isBlocked,
+        role: { key: role.key, permissions: role.permissions },
+      });
+
+      (req as any).session.access = accessToken;
+      (req as any).loggedUser = verify(accessToken, process.env.JWT_KEY!) as IjwtPayload;
+
+      next();
+
+    } catch (error) {
+      return res.status(423).json({ message: 'token expired' });
+    }
   }
-  next();
 };
 
 
@@ -52,3 +62,4 @@ export const generateAccessToken = (payload: IjwtPayload) =>
   jwt.sign(payload, process.env.JWT_KEY!, {
     expiresIn: process.env.NODE_ENV === 'development' ? '1d': '1h',
   });
+
