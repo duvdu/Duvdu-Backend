18ff853bd7e2e80f55a0a541ffa9286285d41e97
motemed-khaled <motemedkhaled5@gmail.com>
2024-07-20 01:33:48 +0300

    update producer contract and add project review

--- analysis/src/routes/index.ts
+++ analysis/src/routes/index.ts
@@ -2,12 +2,16 @@ import { Application } from 'express';
 
 import { router as contractRoutes } from './contract.routes';
 import { router as homeRoutes } from './home.routes';
+import { router as projectReview } from './projectReviews.routes';
 import { router as rankRoutes } from './rank.routes';
 import { router as settingRoutes } from './setting.routes';
+
 
 export const mountRoutes = (app:Application)=>{
   app.use('/api/analysis/rank' , rankRoutes);
   app.use('/api/analysis/home' , homeRoutes);
   app.use('/api/analysis/setting' , settingRoutes);
   app.use('/api/analysis/contract-review' , contractRoutes);
+  app.use('/api/analysis/project-review' , projectReview);
+
 };
