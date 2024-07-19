f6e7c30b49404dddd044018174f1c81d66879aed
motemed-khaled <motemedkhaled5@gmail.com>
2024-07-19 16:46:27 +0300

    refact analysis

--- analysis/src/types/endpoints/projectView.endpoints.ts
+++ analysis/src/types/endpoints/projectView.endpoints.ts
@@ -1,21 +1,21 @@
-import { IProjectReview, PaginationResponse, SuccessResponse } from '@duvdu-v1/duvdu';
-import { RequestHandler } from 'express';
+// import { IProjectReview, PaginationResponse, SuccessResponse } from '@duvdu-v1/duvdu';
+// import { RequestHandler } from 'express';
 
 
 
-export interface CreateProjectReviewHandler
-extends RequestHandler<unknown , SuccessResponse<{data:IProjectReview}> , Pick<IProjectReview , 'comment' | 'rate' | 'project' | 'cycle'> , unknown>{}
+// export interface CreateProjectReviewHandler
+// extends RequestHandler<unknown , SuccessResponse<{data:IProjectReview}> , Pick<IProjectReview , 'comment' | 'rate' | 'project' | 'cycle'> , unknown>{}
 
 
-export interface UpdateProjectViewHandler
-extends RequestHandler<{reviewId:string} , SuccessResponse<{data:IProjectReview}> , Partial<Pick<IProjectReview , 'comment' | 'rate'>> , unknown>{}
+// export interface UpdateProjectViewHandler
+// extends RequestHandler<{reviewId:string} , SuccessResponse<{data:IProjectReview}> , Partial<Pick<IProjectReview , 'comment' | 'rate'>> , unknown>{}
 
 
-export interface GetProjectReviewsHandler
-extends RequestHandler<unknown , PaginationResponse<{data:IProjectReview[]}> , unknown , unknown>{}
+// export interface GetProjectReviewsHandler
+// extends RequestHandler<unknown , PaginationResponse<{data:IProjectReview[]}> , unknown , unknown>{}
 
-export interface GetProjectReviewHandler
-extends RequestHandler<{reviewId:string} , SuccessResponse<{data:IProjectReview}> , unknown , unknown>{}
+// export interface GetProjectReviewHandler
+// extends RequestHandler<{reviewId:string} , SuccessResponse<{data:IProjectReview}> , unknown , unknown>{}
 
-export interface DeleteReviewHandler
-extends RequestHandler<{reviewId:string} , SuccessResponse , unknown , unknown>{}
+// export interface DeleteReviewHandler
+// extends RequestHandler<{reviewId:string} , SuccessResponse , unknown , unknown>{}
