b6e6f3c719e043838b4e95e5b088d8185fa2d1b9
motemed-khaled <motemedkhaled5@gmail.com>
2024-07-19 20:26:24 +0300

    handle user rank

--- auth/src/controllers/auth/get-logged-user-profile.controller.ts
+++ auth/src/controllers/auth/get-logged-user-profile.controller.ts
@@ -1,15 +1,20 @@
 import { UnauthenticatedError, Users } from '@duvdu-v1/duvdu';
 
+import { updateRankForUser } from '../../services/rank.service';
 import { GetLoggedUserProfileHandler } from '../../types/endpoints/user.endpoints';
 
 export const getLoggedUserProfileHandler: GetLoggedUserProfileHandler = async (req, res, next) => {
-  
+
+  await updateRankForUser(req.loggedUser.id , req.lang);
   const user = await Users.findById(req.loggedUser?.id)
     .select(
       '-googleId -appleId -password -verificationCode.code -verificationCode.expireAt -refreshTokens -role -favourites',
     ).populate({path:'category' , select:'title'})
     .lean();
   if (!user) return next(new UnauthenticatedError(undefined, req.lang));
+
+
+
   (user as any).averageRate = +(
     user.rate.ratersCounter > 0 ? user.rate.totalRates / user.rate.ratersCounter : 0
   ).toFixed(2);
