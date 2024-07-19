89a6b0feba7f36e572ffe95b745a202ff07c91db
motemed-khaled <motemedkhaled5@gmail.com>
2024-07-19 21:00:43 +0300

    add rank color

--- common/src/models/ranks.model.ts
+++ common/src/models/ranks.model.ts
@@ -7,10 +7,12 @@ import { MODELS } from '../types/model-names';
 export interface Irank {
     actionCount:number;
     rank:string;
+    color:string
 }
 
 
 export const Rank = model<Irank>(MODELS.rank , new Schema<Irank>({
   actionCount:{type:Number , unique:true , default:0},
-  rank:{type:String , unique:true , default:null}
+  rank:{type:String , unique:true , default:null},
+  color:{type:String , default:null}
 } , {timestamps:true , collection:MODELS.rank}));
