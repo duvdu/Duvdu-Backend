28b2c780201781291356f906469ad318fb92e78a
motemed-khaled <motemedkhaled5@gmail.com>
2024-08-22 11:36:52 +0300

    update studio-booking to rentals

--- common/src/types/cycles.ts
+++ common/src/types/cycles.ts
@@ -1,6 +1,6 @@
 export enum CYCLES {
   portfolioPost = 'project',
-  studioBooking = 'studio-booking',
+  studioBooking = 'rentals',
   copyRights = 'copy-rights',
   teamProject = 'team-project',
   producer = 'producer'
