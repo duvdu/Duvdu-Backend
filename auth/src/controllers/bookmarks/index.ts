ab1cd756d2b9515ecc557f32e5d89a6853fe902a
mohamedelewasy <eng.m.elewasy@gmail.com>
2024-09-26 14:19:02 +0300

    remove old bookmarks

--- auth/src/controllers/bookmarks/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './add-project.controller';
-export * from './create-bookmark-list.controller';
-export * from './get-bookmark.controller';
-export * from './get-bookmarks.controller';
-export * from './remove-project.controller';
-export * from './remove-bookmark.controller';
-export * from './update-bookmark.controller';
