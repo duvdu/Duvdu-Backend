874ba0a88d7329966071b1f0cb1aac11debeaacc
C-YasserElkholy <motemedkhaled5@gmail.com>
2024-12-26 18:01:46 +0200

    fix generate six digits

--- auth/src/utils/gitRandom6Dugut.ts
+++ auth/src/utils/gitRandom6Dugut.ts
@@ -1,4 +1,5 @@
 export function generateRandom6Digit(): string {
   const randomNum = Math.floor(100000 + Math.random() * 900000).toString();
-  return randomNum;
+  // return randomNum;
+  return '123456';
 }
