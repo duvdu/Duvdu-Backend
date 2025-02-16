c1ddbd78a82521bed27feea4d529b1100152ee1a
motemed khaled <motemedkhaled5@gmail.com>
2025-02-16 20:07:50 +0200

    fix update rental project issue

--- contracts/src/controllers/get-contracts.controller.ts
+++ contracts/src/controllers/get-contracts.controller.ts
@@ -174,6 +174,9 @@ export const getContracts: RequestHandler<
           username: '$customer.username',
           isOnline: '$customer.isOnline',
           profileImage: '$customer.profileImage',
+          faceRecognition: '$customer.faceRecognition',
+          email: '$customer.email',
+          phoneNumber: '$customer.phoneNumber',
         },
         sp: {
           _id: '$sp._id',
@@ -182,6 +185,8 @@ export const getContracts: RequestHandler<
           isOnline: '$sp.isOnline',
           profileImage: '$sp.profileImage',
           faceRecognition: '$sp.faceRecognition',
+          email: '$sp.email',
+          phoneNumber: '$sp.phoneNumber',
         },
       },
     },
