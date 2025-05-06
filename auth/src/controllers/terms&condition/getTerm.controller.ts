d86dc0a62d3e5b65b61c4968c790378ec2da38f2
motemed khaled <motemedkhaled5@gmail.com>
2025-05-07 01:39:30 +0200

    update terms and conditions to support loclization

--- auth/src/controllers/terms&condition/getTerm.controller.ts
+++ auth/src/controllers/terms&condition/getTerm.controller.ts
@@ -4,7 +4,14 @@ import { NotFound, Term } from '@duvdu-v1/duvdu';
 import { GetTermHandler } from '../../types/endpoints/terms.endpoints';
 
 export const getTermHandler: GetTermHandler = async (req, res, next) => {
-  const term = await Term.find();
+  const term = await Term.aggregate([
+    {
+      $project: {
+        _id: 1,
+        desc: `$desc.${req.lang}`,
+      },
+    },
+  ]);
   if (term.length === 0) return next(new NotFound(undefined, req.lang));
   res.status(200).json({ message: 'success', data: term[0] });
 };
--- auth/src/validators/terms&condition/createTerm.val.ts
+++ auth/src/validators/terms&condition/createTerm.val.ts
@@ -2,12 +2,14 @@ import { globalValidatorMiddleware } from '@duvdu-v1/duvdu';
 import { check } from 'express-validator';
 
 export const createTermVal = [
-  check('desc').exists().isString().notEmpty().withMessage('descRequired'),
+  check('desc.en').exists().isString().notEmpty().withMessage('descRequired'),
+  check('desc.ar').exists().isString().notEmpty().withMessage('descRequired'),
   globalValidatorMiddleware,
 ];
 
 export const updateTermVal = [
   check('termId').isMongoId().withMessage('termIdInvalid'),
-  check('desc').exists().isString().notEmpty().withMessage('descRequired'),
+  check('desc.en').exists().isString().notEmpty().withMessage('descRequired'),
+  check('desc.ar').exists().isString().notEmpty().withMessage('descRequired'),
   globalValidatorMiddleware,
 ];
