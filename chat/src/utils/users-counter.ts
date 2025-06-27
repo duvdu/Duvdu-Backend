0d4e6a24a45ec76b4aa1c7cd3679c506a0b3bd3e
motemed khaled <motemedkhaled5@gmail.com>
2025-06-27 22:07:49 +0300

    Refactor users-counter.ts to utilize a single Redis client instance, improving performance and reducing redundant connection calls.

--- chat/src/utils/users-counter.ts
+++ chat/src/utils/users-counter.ts
@@ -1,36 +1,25 @@
-import { redisConnection } from '@duvdu-v1/duvdu';
+import { getRedisClient } from '@duvdu-v1/duvdu';
 
 import { totalLogged, totalVisitors } from '../types/socket-events';
 
-let redisClient: Awaited<ReturnType<typeof redisConnection>> | null = null;
-
-const getRedisClient = async () => {
-  if (!redisClient) {
-    redisClient = await redisConnection('', ' ');
-  }
-  return redisClient;
-};
+const redisClient = getRedisClient();
 
 export const addUserToLogged = async (n = 1) => {
-  const client = await getRedisClient();
-  const count = +((await client.get(totalLogged)) || 0);
-  await client.set(totalLogged, count + n);
+  const count = +((await redisClient.get(totalLogged)) || 0);
+  await redisClient.set(totalLogged, count + n);
   return count + n;
 };
 
 export const addUserToVisitor = async (n = 1) => {
-  const client = await getRedisClient();
-  const count = +((await client.get(totalVisitors)) || 0);
-  await client.set(totalVisitors, count + n);
+  const count = +((await redisClient.get(totalVisitors)) || 0);
+  await redisClient.set(totalVisitors, count + n);
   return count + n;
 };
 
 export const getLoggedCount = async () => {
-  const client = await getRedisClient();
-  return +((await client.get(totalLogged)) || 0);
+  return +((await redisClient.get(totalLogged)) || 0);
 };
 
 export const getVisitorCount = async () => {
-  const client = await getRedisClient();
-  return +((await client.get(totalVisitors)) || 0);
+  return +((await redisClient.get(totalVisitors)) || 0);
 };
