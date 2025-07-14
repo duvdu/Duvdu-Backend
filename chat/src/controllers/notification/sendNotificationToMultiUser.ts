51ed2d13c9f86758066db483eeaf8b85b45187ff
motemed khaled <motemedkhaled5@gmail.com>
2025-07-15 01:27:46 +0300

    Refactor notification handling to improve FCM notification service integration, including renaming utility function and enhancing notification creation and population logic.

--- chat/src/controllers/notification/sendNotificationToMultiUser.ts
+++ chat/src/controllers/notification/sendNotificationToMultiUser.ts
@@ -3,7 +3,7 @@ import 'express-async-errors';
 import { BadRequestError, Notification, Users } from '@duvdu-v1/duvdu';
 
 import { SendNotificationMultiUserHandler } from '../../types/endpoints/notification.endpoint';
-import { sendFcmToMultipleUsers } from '../../utils/sendFcmToMultiUser';
+import { sendFCMNotification } from '../../utils/sendNotificationOrFcm';
 
 export const sendNotificationToMultiUserHandler: SendNotificationMultiUserHandler = async (
   req,
@@ -22,24 +22,32 @@ export const sendNotificationToMultiUserHandler: SendNotificationMultiUserHandle
       .flatMap((user) => user.fcmTokens.map((token) => token.fcmToken))
       .filter((token) => token !== null && token !== undefined);
 
+    let notificationId: any = null;
     // save notification to database
     for (const user of users) {
-      await Notification.create({
+      const notification = await Notification.create({
         title: req.body.title,
         message: req.body.message,
         target: user._id,
         sourceUser: req.loggedUser?.id,
         targetUser: req.loggedUser?.id,
       });
+
+      notificationId = notification._id;
     }
 
+    const populatedNotification = await Notification.findById(notificationId!).populate({
+      path: 'targetUser',
+      select: 'name username',
+    });
+
     if (notificationTokens.length > 0) {
-      const fcmResult = await sendFcmToMultipleUsers(
+      await sendFCMNotification(
         notificationTokens,
         req.body.title,
         req.body.message,
+        populatedNotification!,
       );
-      console.log('FCM notification result:', fcmResult);
     }
 
     res.status(200).json({ message: 'success' });
