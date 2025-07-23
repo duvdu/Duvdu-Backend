01ba6c9062bc8330f27e09f3273c9980fb795400
motemed khaled <motemedkhaled5@gmail.com>
2025-07-23 17:54:29 +0300

    update pages schema

--- common/src/models/pages.model.ts
+++ common/src/models/pages.model.ts
@@ -3,16 +3,16 @@ import { model, Schema } from 'mongoose';
 import { MODELS } from '../types/model-names';
 
 export interface IPage {
-  title: string;
-  content: string;
+  title: { ar: string; en: string };
+  content: { ar: string; en: string };
 }
 
 export const Pages = model<IPage>(
   MODELS.page,
   new Schema(
     {
-      title: { type: String, default: null },
-      content: { type: String, default: null },
+      title: { ar: { type: String, default: null }, en: { type: String, default: null } },
+      content: { ar: { type: String, default: null }, en: { type: String, default: null } },
     },
     {
       timestamps: true,
