1f654965d945e1ab15ddcfe74034a71d3f1d3082
motemed khaled <motemedkhaled5@gmail.com>
2025-07-23 14:20:43 +0300

    Remove fundingTransactions.routes and integrate its functionality into transactions.routes for improved organization and maintainability.

--- payment/src/routes/index.ts
+++ payment/src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Application } from 'express';
 
-import { router as fundingTransactionsRoutes } from './fundingTransactions.routes';
 import { router as paymobRoutes } from './paymob.routes';
 import { router as transactionsRoutes } from './transactions.routes';
 
 export const mountRoutes = (app: Application) => {
-  app.use('/api/payment/funding-transactions', fundingTransactionsRoutes);
   app.use('/api/payment/transactions', transactionsRoutes);
   app.use('/api/payment', paymobRoutes);
 };
--- payment/src/routes/transactions.routes.ts
+++ payment/src/routes/transactions.routes.ts
@@ -9,8 +9,11 @@ import {
 } from '@duvdu-v1/duvdu';
 import express from 'express';
 
+import * as controller from '../controllers/fundTransactions';
 import * as transactionsControllers from '../controllers/transactions';
+import * as validation from '../validators/fundedTransactions.validation';
 import * as transactionsValidators from '../validators/transactions.validation';
+
 
 export const router = express.Router();
 
@@ -42,6 +45,33 @@ router
     checkRequiredFields({ fields: ['fundAttachment'] }),
     transactionsValidators.fundTransactionValidation,
     transactionsControllers.fundTransactions,
+  );
+
+router
+  .route('/crm')
+  .post(
+    isauthorized(PERMISSIONS.createFundTransactions),
+    globalUploadMiddleware(FOLDERS.transactions, {
+      maxSize: 400 * 1024 * 1024,
+      fileTypes: ['video/*', 'image/*', 'audio/*', 'application/*'],
+    }).fields([{ name: 'fundAttachment', maxCount: 1 }]),
+    checkRequiredFields({ fields: ['fundAttachment'] }),
+    validation.createFundedTransactionValidation,
+    controller.createFundTransactions,
+  )
+  .get(
+    isauthorized(PERMISSIONS.listFundTransactions),
+    globalPaginationMiddleware,
+    validation.getFundingTransactionPaginationValidation,
+    controller.getFundingTransactionPagination,
+  );
+
+router
+  .route('/crm/:transactionId')
+  .get(
+    isauthorized(PERMISSIONS.listFundTransactions),
+    validation.getFundingTransactionValidation,
+    controller.getFundingTransaction,
   );
 
 router
