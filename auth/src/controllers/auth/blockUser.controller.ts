9c308f0a0cd6e576c1f0fd321ebaf11aad57c4f0
motemed khaled <motemedkhaled5@gmail.com>
2025-07-27 17:00:48 +0300

    Update blockUserHandler to clear refresh tokens upon user blocking and adjust token expiration to 1 minute in generateAccessToken function

--- auth/src/controllers/auth/blockUser.controller.ts
+++ auth/src/controllers/auth/blockUser.controller.ts
@@ -14,7 +14,8 @@ export const blockUserHandler: RequestHandler<
     return next(new NotFound({ en: 'user not found', ar: 'لم يتم العثور على المستخدم' }, req.lang));
 
   user.isBlocked = { reason: req.body.reason, value: true };
-
+  user.refreshTokens = [];
   await user.save();
+
   res.status(200).json({ message: 'success' });
 };
--- auth/src/utils/generateToken.ts
+++ auth/src/utils/generateToken.ts
@@ -5,7 +5,7 @@ import { env } from '../config/env';
 
 export const generateAccessToken = (payload: IjwtPayload) =>
   jwt.sign(payload, env.jwt.secret, {
-    expiresIn: env.environment === 'development' ? '1d' : '1h',
+    expiresIn: '1m', 
   });
 
 export const generateRefreshToken = (payload: { id: string }) =>
