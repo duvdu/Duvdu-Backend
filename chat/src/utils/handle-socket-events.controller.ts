3a8160d4a6e999de8eba0e2e653435f3b6167121
motemed khaled <motemedkhaled5@gmail.com>
2025-08-05 19:14:41 +0300

    Remove user role check from visitors counter event in handleSocketEvents controller

--- chat/src/utils/handle-socket-events.controller.ts
+++ chat/src/utils/handle-socket-events.controller.ts
@@ -7,9 +7,6 @@ import { EVENTS } from '../types/socket-events';
 export const handleSocketEvents = (io: Server, socket: Socket) => {
   const userRole = socket.data.role as Irole | undefined;
   socket.on(EVENTS.getVisitorsCounter, async () => {
-    console.log(userRole);
-    if (userRole?.key !== SystemRoles.admin)
-      return socket.emit(EVENTS.error, { message: 'not allowed' });
     socket.emit(EVENTS.visitorsCounterUpdate, { counter: await getVisitorCount() });
   });
 
