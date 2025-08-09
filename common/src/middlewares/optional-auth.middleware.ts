5b454f14965931a0a5ea9deb112376b3938f0561
motemed khaled <motemedkhaled5@gmail.com>
2025-08-09 17:20:21 +0300

    update optional auth guard

--- common/src/middlewares/optional-auth.middleware.ts
+++ common/src/middlewares/optional-auth.middleware.ts
@@ -1,17 +1,27 @@
 import { RequestHandler } from 'express';
-import { verify } from 'jsonwebtoken';
+import jwt, { verify } from 'jsonwebtoken';
 
 import { UnauthorizedError } from '../errors/unauthorized-error';
+import { Roles } from '../models/Role.model';
+import { Users } from '../models/User.model';
 import { IjwtPayload } from '../types/JwtPayload';
 
 export const optionalAuthenticated: RequestHandler = async (req, res, next) => {
   if (!(req as any).session.access) return next();
 
   let payload: IjwtPayload;
+
   try {
     payload = <IjwtPayload>verify((req as any).session.access, process.env.JWT_KEY!);
     (req as any).loggedUser = payload;
-    if ((req as any).loggedUser.isBlocked.value)
+    const user = await Users.findById(payload.id);
+    if (!user) {
+      return next(
+        new UnauthorizedError({ en: 'user not found', ar: 'لا يوجد مستخدم' }, (req as any).lang),
+      );
+    }
+
+    if (user.isBlocked.value)
       return next(
         new UnauthorizedError(
           {
@@ -22,12 +32,63 @@ export const optionalAuthenticated: RequestHandler = async (req, res, next) => {
         ),
       );
 
-    if ((req as any).loggedUser.isDeleted)
+    if (user.isDeleted)
       return next(
         new UnauthorizedError({ en: 'user not found', ar: 'لا يوجد مستخدم' }, (req as any).lang),
       );
+
+    next();
   } catch (error) {
-    return next();
+    try {
+      const payload = <{ id: string }>verify((req as any).session.refresh, process.env.JWT_KEY!);
+      const user = await Users.findOne({
+        _id: payload.id,
+        refreshTokens: {
+          $elemMatch: {
+            token: (req as any).session.refresh,
+          },
+        },
+      });
+      if (!user) return res.status(423).json({ message: 'token expired' });
+
+      if (user.isBlocked.value)
+        return next(
+          new UnauthorizedError(
+            {
+              en: `Forbidden: User is blocked ${(req as any).loggedUser.isBlocked.reason}`,
+              ar: ` ممنوع: المستخدم محظور ${(req as any).loggedUser.isBlocked.reason}`,
+            },
+            (req as any).lang,
+          ),
+        );
+
+      if (user.isDeleted)
+        return next(
+          new UnauthorizedError({ en: 'user not found', ar: 'لا يوجد مستخدم' }, (req as any).lang),
+        );
+
+      const role = await Roles.findById(user.role);
+      if (!role) return res.status(423).json({ message: 'invalid role' });
+
+      const accessToken = generateAccessToken({
+        id: user.id,
+        isVerified: user.isVerified,
+        isBlocked: user.isBlocked,
+        role: { key: role.key, permissions: role.permissions },
+      });
+
+      (req as any).session.access = accessToken;
+      (req as any).session.refresh = (req as any).session.refresh;
+      (req as any).loggedUser = verify(accessToken, process.env.JWT_KEY!) as IjwtPayload;
+
+      next();
+    } catch (error) {
+      return res.status(423).json({ message: 'token expired' });
+    }
   }
-  next();
 };
+
+export const generateAccessToken = (payload: IjwtPayload) =>
+  jwt.sign(payload, process.env.JWT_KEY!, {
+    expiresIn: process.env.NODE_ENV === 'development' ? '1d' : '1h',
+  });
