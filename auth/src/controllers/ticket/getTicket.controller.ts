85217167382819523d0ff55825022017bed88b36
motemed khaled <motemedkhaled5@gmail.com>
2025-10-06 22:49:13 +0300

    Enhance getTicketHandler to include user and closedBy details in ticket retrieval, improving response data richness for better client-side handling.

--- auth/src/controllers/ticket/getTicket.controller.ts
+++ auth/src/controllers/ticket/getTicket.controller.ts
@@ -4,7 +4,15 @@ import { NotFound, Ticket } from '@duvdu-v1/duvdu';
 import { GetTicketHandler } from '../../types/endpoints/ticket.endpoints';
 
 export const getTicketHandler: GetTicketHandler = async (req, res, next) => {
-  const ticket = await Ticket.findById(req.params.ticketId);
+  const ticket = await Ticket.findById(req.params.ticketId).populate({
+    path: 'userId',
+    select: 'name username email profileImage',
+  })
+    .populate({
+      path: 'state.closedBy',
+      select: 'name username email profileImage',
+    });
+  
   if (!ticket) return next(new NotFound(undefined, req.lang));
   res.status(200).json({ message: 'success', data: ticket });
 };
