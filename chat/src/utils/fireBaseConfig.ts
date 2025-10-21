ea71fde6cda482aac1611f73c6ec99037d643890
motemed khaled <motemedkhaled5@gmail.com>
2025-10-21 22:12:29 +0300

    update infar yaml

--- chat/src/utils/fireBaseConfig.ts
+++ chat/src/utils/fireBaseConfig.ts
@@ -1,9 +1,8 @@
 import * as admin from 'firebase-admin';
 
-import serviceAccount from '../serviceAccount.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(process.env.FIREBASE_KEY_JSON as admin.ServiceAccount),
 });
 
 export default admin;
